8ef8f8dd6dbde5fb64f40b8b1fbeb63e05108751
maciejspychala <mac.spychala@gmail.com>
2018-09-23 19:59:29 +0100

    change example

--- src/main.c
+++ src/main.c
@@ -2,11 +2,20 @@
 #include "net.h"
 
 int main() {
-    struct Genome *genome = new_genome(5, 3);
+    struct Genome *genome = new_genome(3, 1);
+    evolve_gene(genome, 2, 4);
+    add_data(find_node(genome, 5)->in_genes, new_gene(1, 5));
+    add_data(find_node(genome, 5)->in_genes, new_gene(4, 5));
     struct ListItem *node = genome->nodes->head;
-    evolve_gene(genome, 4, 8);
-    float input[5] = { 1.0, 0.4, 0.2, 0.5, 0.8 };
-    calculate_output(genome, input, 5);
+    float in[3] = { 1, 0.5, 0.2 };
+    calculate_output(genome, in, 3);
+    while (node) {
+        print_node((struct Node*) node->data);
+        node = node->next;
+    }
+    printf("\n\n");
+    calculate_output(genome, in, 3);
+    node = genome->nodes->head;
     while (node) {
         print_node((struct Node*) node->data);
         node = node->next;
